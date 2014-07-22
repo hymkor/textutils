887e656244139641bc2cc369133c375069ac4210
HAYAMA_Kaoru <iyahaya@nifty.com>
2014-07-22 18:23:42 +0900

    WriteString->fmt.Fprintln , defer はプログラム終了時までファイルをつかみっぱなしになるので、使用後すぐ手動でクローズするようにした。

--- ansistrip.go
+++ ansistrip.go
@@ -4,32 +4,29 @@ import "bufio"
 import "io"
 import "os"
 import "regexp"
+import "fmt"
 
 func cat1(r io.Reader, ansiStrip *regexp.Regexp) {
 	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
-		text := ansiStrip.ReplaceAllString(scanner.Text(), "")
-		os.Stdout.WriteString(text)
-		os.Stdout.WriteString("\n")
+		fmt.Println(ansiStrip.ReplaceAllString(scanner.Text(), ""))
 	}
 }
 
 func main() {
 	ansiStrip, err := regexp.Compile("\x1B[^a-zA-Z]*[A-Za-z]")
 	if err != nil {
-		os.Stderr.WriteString(err.Error())
-		os.Stderr.WriteString("\n")
+		fmt.Fprintln(os.Stderr, err.Error())
 		return
 	}
 	for _, arg1 := range os.Args[1:] {
 		r, err := os.Open(arg1)
 		if err != nil {
-			os.Stderr.WriteString(err.Error())
-			os.Stderr.WriteString("\n")
+			fmt.Fprintln(os.Stderr, err.Error())
 			return
 		}
-		defer r.Close()
 		cat1(r, ansiStrip)
+		r.Close()
 	}
 	if len(os.Args) <= 1 {
 		cat1(os.Stdin, ansiStrip)
