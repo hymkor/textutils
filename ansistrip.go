7042a3e98e4098be9e3a2dd896343b7672c185ca
HAYAMA_Kaoru <iyahaya@nifty.com>
2014-07-22 18:32:21 +0900

    正規表現オブジェクトを素直にグローバルにした。

--- ansistrip.go
+++ ansistrip.go
@@ -6,7 +6,9 @@ import "os"
 import "regexp"
 import "fmt"
 
-func cat1(r io.Reader, ansiStrip *regexp.Regexp) {
+var ansiStrip = regexp.MustCompile("\x1B[^a-zA-Z]*[A-Za-z]")
+
+func cat1(r io.Reader) {
 	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
 		fmt.Println(ansiStrip.ReplaceAllString(scanner.Text(), ""))
@@ -14,21 +16,16 @@ func cat1(r io.Reader, ansiStrip *regexp.Regexp) {
 }
 
 func main() {
-	ansiStrip, err := regexp.Compile("\x1B[^a-zA-Z]*[A-Za-z]")
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
-		return
-	}
 	for _, arg1 := range os.Args[1:] {
 		r, err := os.Open(arg1)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err.Error())
 			return
 		}
-		cat1(r, ansiStrip)
+		cat1(r)
 		r.Close()
 	}
 	if len(os.Args) <= 1 {
-		cat1(os.Stdin, ansiStrip)
+		cat1(os.Stdin)
 	}
 }
