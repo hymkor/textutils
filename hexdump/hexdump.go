b0827dad5b23ae3a583c8f56814ffd2fefc3a928
HAYAMA_Kaoru <iyahaya@nifty.com>
2014-12-28 23:34:52 +0900

    hexdump support stdin

--- hexdump/hexdump.go
+++ hexdump/hexdump.go
@@ -6,27 +6,36 @@ import (
 	"os"
 )
 
+func dump(fd io.Reader) {
+	var buffer [16]byte
+	for {
+		n, nErr := fd.Read(buffer[:])
+		if nErr != nil {
+			if nErr != io.EOF {
+				fmt.Fprintln(os.Stderr, nErr.Error())
+			}
+			break
+		}
+		for i := 0; i < n; i++ {
+			fmt.Printf("%02X ", buffer[i])
+		}
+		fmt.Println()
+	}
+}
+
 func main() {
+	count := 0
 	for _, fname := range os.Args[1:] {
 		fd, fdErr := os.Open(fname)
 		if fdErr != nil {
 			fmt.Fprintln(os.Stderr, fdErr.Error())
 			return
 		}
-		defer fd.Close()
-		var buffer [16]byte
-		for {
-			n, nErr := fd.Read(buffer[:])
-			if nErr != nil {
-				if nErr != io.EOF {
-					fmt.Fprintln(os.Stderr, nErr.Error())
-				}
-				break
-			}
-			for i := 0; i < n; i++ {
-				fmt.Printf("%02X ", buffer[i])
-			}
-			fmt.Println()
-		}
+		dump(fd)
+		fd.Close()
+		count++
+	}
+	if count <= 0 {
+		dump(os.Stdin)
 	}
 }
