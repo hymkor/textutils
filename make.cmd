a596edba0f5496d9fc9a6ca73e537be3eb304952
HAYAMA_Kaoru <iyahaya@nifty.com>
2014-12-29 20:51:47 +0900

    make.cmd install <INSTALLDIR>

--- make.cmd
+++ make.cmd
@@ -1,5 +1,7 @@
 if "%1" == "clean" (
         for /R . %%I in (*~) do del %%I
+) else if "%1" == "install" (
+        if not "%2" == "" for /R . %%I in (*.exe) do copy "%%I" "%2"\.
 ) else (
         for %%I in (ansistrip camelfmt cure hexdump) do (pushd %%I & go build & popd)
 )
